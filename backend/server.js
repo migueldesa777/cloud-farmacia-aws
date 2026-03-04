22c70725cec58b6b0949ed1a53925db1393fe6e1
migueldesa777 <miguelotaviodesa7@gmail.com>
2026-03-04 19:10:55 -0300

    Initialize server with log and route comments
    Add initial server setup with console log and comments

---
+++ backend/server.js
@@ -0,0 +1,5 @@
+console.log("Servidor da Farmácia Online rodando");
+
+// Aqui você pode colocar comentários explicando:
+// - Quais rotas teriam (ex.: /usuarios, /produtos, /vendas)
+// - Como seria a conexão com o banco de dados
